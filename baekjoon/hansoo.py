68d2289992e3b4dbbac4f10a789e43331b79d44e
Yongsung Kang <yongsung.kang@gmail.com>
2016-09-05 23:46:51 +0900

    solution of hansoo

---
+++ baekjoon/hansoo.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+def isArithmeticSequence(n):
+    seq = list(map(int,list(str(n))))
+    if len(seq) < 3:
+        return True
+    a = seq.pop()
+    b = seq.pop()
+    baseDiff = a - b
+    while seq:
+        a = seq.pop()
+        currDiff = b - a
+        if baseDiff != currDiff:
+            return False
+        b = a
+    return True
+
+print(len(list(filter(isArithmeticSequence, [x + 1 for x in range(n)]))))
+
