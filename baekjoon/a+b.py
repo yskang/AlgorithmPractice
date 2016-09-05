a02b0d1fc0fc70d157267e1830d7d3de1bccd5f8
Yongsung Kang <yongsung.kang@gmail.com>
2016-09-05 11:47:43 +0900

    add baekjoon solutions

---
+++ baekjoon/a+b.py
@@ -0,0 +1,15 @@
+import sys
+
+# testData = [
+#     '1 2'
+# ]
+
+# testData.reverse()
+# rl = lambda: testData.pop()
+rl = lambda: input()
+
+line = rl()
+a = int(line.split(' ')[0])
+b = int(line.split(' ')[1])
+
+print(a+b)
---
+++ baekjoon/fourFundamentalArithmeticOperations.py
@@ -0,0 +1,19 @@
+import sys
+
+# testData = [
+#     '1 2'
+# ]
+
+# testData.reverse()
+# rl = lambda: testData.pop()
+rl = lambda: input()
+
+line = rl()
+a = int(line.split(' ')[0])
+b = int(line.split(' ')[1])
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(divmod(a,b)[0])
+print(divmod(a,b)[1])
