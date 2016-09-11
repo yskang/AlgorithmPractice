0b442fb6cf4589bdc312b3f7e81a4b848a6fd663
Yongsung Kang <yongsung.kang@gmail.com>
2016-09-11 20:54:04 +0900

    I tried many different ways to sort method but the real problem is not a sorting method but a input function. sys.stdin.readline() is more faster than input().

--- baekjoon/sortNumber.py
+++ baekjoon/sortNumber.py
@@ -1,28 +1,78 @@
+import sys
+
 testData = [
-'5',
-'5',
-'2',
-'3',
+'12',
+'1',
+'100',
+'-3',
 '4',
-'1'
+'0',
+'1000000',
+'-9',
+'8',
+'7',
+'6',
+'-5',
+'-1000000'
 ]
 
-testData.reverse()
+def quickSort(xs):
+    if len(xs) <= 1:
+        return xs
 
+    pivot = xs[len(xs)-1]
+
+    less = []
+    more = []
+    equal = []
+
+    for x in xs:
+        if x < pivot:
+            less.append(x)
+        elif x > pivot:
+            more.append(x)
+        else:
+            equal.append(x)
+
+    return quickSort(less) + equal + quickSort(more)
+
+def mergeSort(xs):
+    if len(xs) <= 1:
+        return xs
+
+    mid = len(xs) // 2
+    lefts = mergeSort(xs[:mid])
+    rights = mergeSort(xs[mid:])
+
+    result = []
+    l = 0
+    r = 0
+
+    while l < len(lefts) and r < len(rights):
+        if lefts[l] < rights[r]:
+            result.append(lefts[l])
+            l += 1
+        else:
+            result.append(rights[r])
+            r += 1
+    
+    result += lefts[l:]
+    result += rights[r:]
+    return result
+
+
+testData.reverse()
 rl = lambda: testData.pop()
-# rl = lambda: input()
+
+# rl = lambda: sys.sdein.readline()
 
 numOfNums = int(rl())
+temp = int(rl())
+nums = {temp}
 
-nums = []
-nums.append(int(rl()))
 for i in range(numOfNums-1):
-    num = int(rl())
-    for j in range(len(nums)):
-        if num < nums[j]:
-            nums.insert(j, num)
-            break 
+    nums.add(int(rl()))
 
-nums.reverse()
-for num in nums:
-    print(num)
+x = sorted(nums)
+
+print('\n'.join(map(str, x)))
