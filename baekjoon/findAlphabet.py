bead81f3ab11eb3278746fe1086a8bf5d14feb68
Kang, Yongsung <yongsung.kang@gmail.com>
2016-09-13 09:11:13 +0900

    Update findAlphabet.py

--- baekjoon/findAlphabet.py
+++ baekjoon/findAlphabet.py
@@ -1,3 +1,7 @@
+# Find Alphabet
+# https://www.acmicpc.net/problem/10809
+
+
 inputWord = 'baekjoon'.strip()
 alphabets = map(chr, range(97, 123))
 print(' '.join(list(map(lambda c: str(inputWord.find(c)), alphabets))))
