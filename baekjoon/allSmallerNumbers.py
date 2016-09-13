eb014c2987572f0933f8a4f7601cd504a608b783
Kang, Yongsung <yongsung.kang@gmail.com>
2016-09-13 09:02:25 +0900

    Update allSmallerNumbers.py

--- baekjoon/allSmallerNumbers.py
+++ baekjoon/allSmallerNumbers.py
@@ -1,2 +1,5 @@
+# Numbers that samller than X
+# https://www.acmicpc.net/problem/10871
+
 [numOfInput, n] = list(map(int, input().split(' ')))
 print(' '.join(list(map(str, filter(lambda x: x < n, list(map(int, input().split(' '))))))))
