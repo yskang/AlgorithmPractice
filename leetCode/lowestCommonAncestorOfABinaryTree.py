47796f66f0a6fb29862799f152bc980a1137d227
Yongsung Kang <yongsung.kang@gmail.com>
2018-01-13 21:28:29 +0900

    TLE, with dfs

--- leetCode/lowestCommonAncestorOfABinaryTree.py
+++ leetCode/lowestCommonAncestorOfABinaryTree.py
@@ -10,27 +10,45 @@ class TreeNode(object):
 
 
 class Solution(object):
+    def __init__(self):
+        self.found = 0
+        self.paths = []
+
     def dfs(self, node, route, targetA, targetB):
         if node is None:
             return
 
-        route.append(node.val)
+        if node is targetA or node is targetB:
+            self.found += 1
+            temp = route[:]
+            temp.append(node)
+            self.paths.append(temp)
 
-        self.dfs(node.left, route, targetA, targetB)
-        self.dfs(node.right, route)
+        if self.found is 2:
+            return
+
+        route.append(node)
+        r1 = route[:]
+        r2 = route[:]
+
+        self.dfs(node.left, r1, targetA, targetB)
+        self.dfs(node.right, r2, targetA, targetB)
 
     def lowestCommonAncestor(self, root, p, q):
-        p.left = None
-        p.right = None
-        q.left = None
-        q.right = None
-
         route = []
 
-        self.dfs(root, route, p.val, q.val)
+        self.found = 0
+        self.dfs(root, route, p, q)
 
-
-        return None
+        i = 0
+        while True:
+            if len(self.paths[0]) <= i or len(self.paths[1]) <= i:
+                return self.paths[0][i-1]
+            elif self.paths[0][i] != self.paths[1][i]:
+                return self.paths[0][i-1]
+            else:
+                i += 1
+                continue
 
 
 node_1 = TreeNode(1)
@@ -58,14 +76,43 @@ root.right = node_1
 
 sol = Solution()
 
+print(sol.lowestCommonAncestor(root, node_5, node_1).val)
 
-class Example_Test(unittest.TestCase):
-    def test_sol_1(self):
-        self.assertEqual(sol.lowestCommonAncestor(root, node_5, node_1), node_3, msg="Wrong!")
-    def test_sol_2(self):
-        self.assertEqual(sol.lowestCommonAncestor(root, node_5, node_4), node_5, msg="Wrong!")
+node_1 = TreeNode(1)
+node_0 = TreeNode(0)
+node_8 = TreeNode(8)
+node_7 = TreeNode(7)
+node_4 = TreeNode(4)
+node_3 = TreeNode(3)
+node_5 = TreeNode(5)
+node_2 = TreeNode(2)
+node_6 = TreeNode(6)
+
+node_1.left = node_0
+node_1.right = node_8
+
+node_2.left = node_7
+node_2.right = node_4
+
+node_5.left = node_6
+node_5.right = node_2
+
+root = node_3
+root.left = node_5
+root.right = node_1
+
+sol = Solution()
+
+print(sol.lowestCommonAncestor(root, node_3, node_1).val)
 
 
-
-if __name__ == '__main__':
-    unittest.main()
+# class Example_Test(unittest.TestCase):
+#     def test_sol_1(self):
+#         self.assertEqual(sol.lowestCommonAncestor(root, node_5, node_1), node_3, msg="Wrong!")
+#
+#     def test_sol_2(self):
+#         self.assertEqual(sol.lowestCommonAncestor(root, node_5, node_4), node_5, msg="Wrong!")
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
