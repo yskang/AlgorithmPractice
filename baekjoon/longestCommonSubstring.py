0f86bc85bfc95b7a9f4b8a4e193d3e5524b7f4e8
Yongsung Kang <yongsung.kang@gmail.com>
2018-02-01 20:04:42 +0900

    runtime error

--- baekjoon/longestCommonSubstring.py
+++ baekjoon/longestCommonSubstring.py
@@ -1,28 +1,85 @@
 #
 # https://www.acmicpc.net/problem/9249
 
+from itertools import zip_longest, islice
+
+def getLPC(str, sa, len_a):
+    max_len = 0
+    max_start = 0
+    prev = sa[0]
+    prev_side = False
+    curr_side = False
+    for line in sa[1:]:
+        if prev < len_a:
+            prev_side = False
+        else:
+            prev_side = True
+        if line < len_a:
+            curr_side = False
+        else:
+            curr_side = True
+
+        if prev_side is not curr_side:
+            min_len = min(len(str) - prev, len(str) - line)
+            for i in range(min_len):
+                if str[prev+i] is not str[line+i]:
+                    if i > max_len:
+                        max_len = i
+                        max_start = prev
+                    break
+                if i == min_len-1:
+                    if i+1 > max_len:
+                        max_len = i + 1
+                        max_start = line
+        prev = line
+    return max_len, max_start
+
+def to_int_keys_best(l):
+    """
+    l: iterable of keys
+    returns: a list with integer keys
+    """
+    seen = set()
+    ls = []
+    for e in l:
+        if not e in seen:
+            ls.append(e)
+            seen.add(e)
+    ls.sort()
+    index = {v: i for i, v in enumerate(ls)}
+    return [index[v] for v in l]
+
+def suffix_array_best(s):
+    """
+    suffix array of s
+    O(n * log(n)^2)
+    """
+    n = len(s)
+    k = 1
+    line = to_int_keys_best(s)
+    while max(line) < n - 1:
+        line = to_int_keys_best(
+            [a * (n + 1) + b + 1
+             for (a, b) in
+             zip_longest(line, islice(line, k, None),
+                         fillvalue=-1)])
+        k <<= 1
+    return line
+
+def inverse_array(l):
+    n = len(l)
+    ans = [0] * n
+    for i in range(n):
+        ans[l[i]] = i
+    return ans
+
 str_a = input()
 str_b = input()
 
-if len(str_a) < len(str_b):
-    str_a, str_b = str_b, str_a
+str = str_a + "!" + str_b
+n = len(str)
 
-dp = [0 for _ in range(len(str_a)+1)]
-dp2 = [0 for _ in range(len(str_a)+1)]
-
-max_len, max_end = 0, 0
-
-for i in range(len(str_b)):
-    for j in range(len(str_a)):
-        if str_a[j] is str_b[i]:
-            dp2[j+1] = dp[j] + 1
-            if dp2[j+1] > max_len:
-                max_end = j+1
-                max_len = dp2[j+1]
-        else:
-            dp2[j+1] = 0
-    print(dp2)
-    dp, dp2 = dp2, dp
-
+suffix_array = inverse_array(suffix_array_best(str))
+max_len, max_start = getLPC(str, suffix_array, len(str_a))
 print(max_len)
-print(str_a[max_end-max_len:max_end])
+print(str[max_start:max_start+max_len])
