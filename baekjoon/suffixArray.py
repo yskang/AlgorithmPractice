c689569759cb1b663663e36e53a1b472c9d4f075
Yongsung Kang <yongsung.kang@gmail.com>
2018-02-02 14:39:51 +0900

    add sample source for suffixArray

---
+++ baekjoon/suffixArray.py
@@ -0,0 +1,42 @@
+import time
+from collections import defaultdict, Counter
+
+def get_suffix_array(str):
+    return sorted(range(len(str)), key=lambda i: str[i:])
+
+def sort_bucket(str, bucket, order=1):
+    print("sort_bucket", str, bucket, order)
+    d = defaultdict(list)
+    print("default dick", d) 
+    for i in bucket:
+        key = str[i:i+order]
+        d[key].append(i)
+    result = []
+    print("d", d)
+    for k,v in sorted(d.items()):
+        print("key",k,"value", v)
+        if len(v) > 1:
+            result += sort_bucket(str, v, order*2)
+        else:
+            result.append(v[0])
+    return result
+
+def suffix_array_ManberMyers(str):
+    return sort_bucket(str, (i for i in range(len(str))))
+
+if __name__ == "__main__":
+    # with open("..\MobyDick.txt") as f:
+    #     m = f.read()
+    # str = m#[:100000]
+    str = input()
+    print(len(str))
+#    str = "mississipi"
+    start_time = time.time()
+    x = get_suffix_array(str)
+    end_time = time.time()
+    print("Time for python sort was %g seconds" % (end_time - start_time))
+    start_time = time.time()
+    y = suffix_array_ManberMyers(str)
+    end_time = time.time()
+    #assert(x == y)
+    print("Time for Manber Myers was %g seconds" % (end_time - start_time))
