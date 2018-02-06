4fbe1c5f052a999126418ff2e43acca29cd31e73
Yongsung Kang <yongsung.kang@gmail.com>
2018-02-06 13:25:12 +0900

    2558, a+b, baekjoon

--- baekjoon/11718_print_as_input.py
+++ baekjoon/11718_print_as_input.py
@@ -1,14 +1,15 @@
 # https://www.acmicpc.net/problem/11718
-import sys
+# import sys
 
-k = []
+# k = []
 
-for i in range(100):
-    l = sys.stdin.readline()
-    k.append(l.replace("\n", ""))
-    if l is '\n':
-        break
+# for i in range(100):
+#     l = sys.stdin.readline()
+#     k.append(l.replace("\n", ""))
+#     if l is '\n':
+#         break
 
-for line in k:
-    print(line)
-
+# for line in k:
+#     print(line)
+while True:
+    print(input())
---
+++ baekjoon/2558_A+b.py
@@ -0,0 +1,7 @@
+# https://www.acmicpc.net/problem/2558
+import sys
+
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+
+print(a+b)
