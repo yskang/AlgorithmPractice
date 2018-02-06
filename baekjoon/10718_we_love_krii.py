21575264508bdd80113696709e4cc5e4a89a41af
Yongsung Kang <yongsung.kang@gmail.com>
2018-02-06 11:46:20 +0900

    10178, 11718, baekjoon

---
+++ baekjoon/10718_we_love_krii.py
@@ -0,0 +1,2 @@
+# https://www.acmicpc.net/problem/10718
+print("강한친구 대한육군\n강한친구 대한육군")
---
+++ baekjoon/11718_print_as_input.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/11718
+import sys
+
+k = []
+
+for i in range(100):
+    l = sys.stdin.readline()
+    k.append(l.replace("\n", ""))
+    if l is '\n':
+        break
+
+for line in k:
+    print(line)
+
