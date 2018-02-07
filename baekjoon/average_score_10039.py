e1150ac92976b94809706c595fbe6a953be3e9ce
Yongsung Kang <yongsung.kang@gmail.com>
2018-02-07 16:02:53 +0900

    average score, 10039, baekjoon

---
+++ baekjoon/average_score_10039.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/10039
+import sys
+
+
+def get_average_score(scores):
+    return sum(list(map(lambda x: 40 if x < 40 else x, scores)))/5
+
+
+if __name__ == "__main__":
+    scores = []
+    for i in range(5):
+        scores.append(int(sys.stdin.readline().strip()))
+
+    print(int(get_average_score(scores)))
