d89cf1716b15a2554246b7f1be376273de89a316
Yongsung Kang <yongsung.kang@gmail.com>
2018-02-07 15:46:33 +0900

    change name of test

--- baekjoon/plus_cycle_test.py
+++ baekjoon/plus_cycle_test.py
@@ -1,10 +1,12 @@
 import unittest
 import baekjoon.plus_cycle_1110
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
+
+class PlusCycleTestCase(unittest.TestCase):
+    def test_get_new_number(self):
         ps = baekjoon.plus_cycle_1110.PlusCycle()
         self.assertEqual(ps.get_new_number("01"), "11")
+
 
 if __name__ == '__main__':
     unittest.main()
