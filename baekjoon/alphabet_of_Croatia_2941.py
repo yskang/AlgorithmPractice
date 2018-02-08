5d156a134caf2175397f30ad65bd4fcf044446fc
Yongsung Kang <yongsung.kang@gmail.com>
2018-02-08 12:23:50 +0900

    alphabet of Croatia, use replace string, 2941 of baekjoon

--- baekjoon/alphabet_of_Croatia_2941.py
+++ baekjoon/alphabet_of_Croatia_2941.py
@@ -2,17 +2,10 @@
 
 
 def count_Croatian(line):
-    croatian_dic = {"c=": True, "c-": True, "dz=": True, "d-": True, "lj": True, "nj": True, "s=": True, "z=": True}
-    count = len(line)
-
-    for i in range(len(line)-1):
-        if line[i:i+2] in croatian_dic and croatian_dic[line[i:i+2]]:
-            count -= 1
-        if i+3 <= len(line) and line[i:i+3] in croatian_dic and croatian_dic[line[i:i+3]]:
-            count -= 1
-
-    return count
-
+    croatian_words = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+    for word in croatian_words:
+        line = line.replace(word, "+")
+    return len(line)
 
 if __name__ == "__main__":
     print(count_Croatian(input()))
