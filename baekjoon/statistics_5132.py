23e657c35c56ccaf6fd38c46e9145d2d26c1bfdf
Yongsung Kang <yongsung.kang@gmail.com>
2018-02-13 13:10:52 +0900

    statistics, reduce execute time

--- baekjoon/statistics_5132.py
+++ baekjoon/statistics_5132.py
@@ -1,12 +1,14 @@
 # https://www.acmicpc.net/problem/2108
 import sys
+
 import math
+
 
 def get_average(nums, offset, n):
     s = 0
     for i in range(len(nums)):
         s = s + (i - offset) * nums[i]
-    return round(s/n)
+    return round(s / n)
 
 
 def get_median(nums, offset, n):
@@ -14,7 +16,7 @@ def get_median(nums, offset, n):
     i = 0
     while True:
         s += nums[i]
-        if s > math.floor(n/2):
+        if s > math.floor(n / 2):
             return i - offset
         i += 1
 
@@ -42,11 +44,54 @@ def get_range(nums, OFFSET):
         if nums[i] > 0:
             min = i
             break
-    for i in range(len(nums)-1, -1, -1):
+    for i in range(len(nums) - 1, -1, -1):
         if nums[i] > 0:
             max = i
             break
     return max - min
+
+
+def calc_total(nums, offset, n):
+    has_median = False
+    median = 0
+    count = 0
+    s = 0
+    maxes = []
+    max_count = 0
+    min_num = -1
+    max_num = -1
+    has_min = False
+
+    for i in range(len(nums)):
+        s = s + (i - offset) * nums[i]
+        count += nums[i]
+
+        if has_median is False and count > math.floor(n / 2):
+            median = i - offset
+            has_median = True
+
+        if max_count < nums[i]:
+            max_count = nums[i]
+            maxes = [i]
+        elif max_count == nums[i]:
+            maxes.append(i)
+
+        if has_min is False and nums[i] > 0:
+            min_num = i
+            has_min = True
+
+        if nums[i] > 0:
+            max_num = i
+
+    if len(maxes) > 1:
+        most_frequent_num = sorted(maxes)[1] - OFFSET
+    else:
+        most_frequent_num = maxes[0] - OFFSET
+
+    print(round(s / n))
+    print(median)
+    print(most_frequent_num)
+    print(max_num - min_num)
 
 
 if __name__ == "__main__":
@@ -59,7 +104,8 @@ if __name__ == "__main__":
         n = int(sys.stdin.readline())
         nums[n + OFFSET] += 1
 
-    print(get_average(nums, OFFSET, N))
-    print(get_median(nums, OFFSET, N))
-    print(get_most_frequent_number(nums, OFFSET))
-    print(get_range(nums, OFFSET))
+    # print(get_average(nums, OFFSET, N))
+    # print(get_median(nums, OFFSET, N))
+    # print(get_most_frequent_number(nums, OFFSET))
+    # print(get_range(nums, OFFSET))
+    calc_total(nums, OFFSET, N)
