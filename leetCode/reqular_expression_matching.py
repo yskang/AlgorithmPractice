80cae83031785dd7c635f12e9a07e671c7cb3189
Yongsung Kang <yongsung.kang@gmail.com>
2018-04-23 16:32:02 +0900

    Regular Expression Matching, leetcode

---
+++ leetCode/reqular_expression_matching.py
@@ -0,0 +1,23 @@
+class Solution():
+    def isMatch(self, s, p):
+        memo = {}
+
+        def dp(i, j):
+            if (i, j) not in memo:
+                if j == len(p):
+                    ans = i == len(s)
+                else:
+                    first_match = i < len(s) and p[j] in {s[i], '.'}
+                    if j + 1 < len(p) and p[j + 1] == "*":
+                        ans = dp(i, j + 2) or first_match and dp(i + 1, j)
+                    else:
+                        ans = first_match and dp(i + 1, j + 1)
+                memo[i, j] = ans
+            return memo[i, j]
+
+        return dp(0, 0)
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.isMatch("aaa", "aaaa"))
