48f4c9448a0cfc3a46c4073b9f0257092edf82c5
Yongsung Kang <yongsung.kang@gmail.com>
2018-04-25 13:41:33 +0900

    Factorial - 10872, baekjoon

---
+++ baekjoon/factorial_10872.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline().strip())
+    print(factorial(n))
