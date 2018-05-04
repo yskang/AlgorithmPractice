ee1aa8de97d22c26c0f25a2d15d6b4ebb6b31b23
Yongsung Kang <yongsung.kang@gmail.com>
2018-05-04 17:50:01 +0900

    least common multiple - 13241, baekjoon

---
+++ baekjoon/least_common_multiple_13241.py
@@ -0,0 +1,18 @@
+# Title: 최소공배수
+# Link: https://www.acmicpc.net/problem/13241
+
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+
+def read_list_int():
+    return list(map(int, sys.stdin.readline().strip().split(' ')))
+
+
+def read_single_int():
+    return int(sys.stdin.readline().strip())
+
+
+if __name__ == '__main__':
+    
