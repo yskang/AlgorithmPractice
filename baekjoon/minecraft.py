7be085afac9d0495474f685baf5fbf664a42efdd
Yongsung Kang <yongsung.kang@gmail.com>
2018-05-08 20:34:44 +0900

    sum of sequence - 1024, baekjoon

---
+++ baekjoon/minecraft.py
@@ -0,0 +1,18 @@
+# Title: 미네크래프트 
+# Link: https://www.acmicpc.net/contest/problem/289/2
+
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+
+def read_list_int():
+    return list(map(int, sys.stdin.readline().strip().split(' ')))
+
+
+def read_single_int():
+    return int(sys.stdin.readline().strip())
+
+
+if __name__ == '__main__':
+    
---
+++ baekjoon/sum_of_sequence_1024.py
@@ -0,0 +1,18 @@
+# Title: 수열의 합 
+# Link: https://www.acmicpc.net/problem/1024
+
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+
+def read_list_int():
+    return list(map(int, sys.stdin.readline().strip().split(' ')))
+
+
+def read_single_int():
+    return int(sys.stdin.readline().strip())
+
+
+if __name__ == '__main__':
+    
