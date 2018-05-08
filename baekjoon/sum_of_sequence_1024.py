6667e3a505b117c3f109410a1293a4f602b31790
Yongsung Kang <yongsung.kang@gmail.com>
2018-05-08 20:35:30 +0900

    sum of sequence - 1024, baekjoon

--- baekjoon/sum_of_sequence_1024.py
+++ baekjoon/sum_of_sequence_1024.py
@@ -1,4 +1,4 @@
-# Title: 수열의 합 
+# Title: 수열의 합
 # Link: https://www.acmicpc.net/problem/1024
 
 import sys
@@ -14,5 +14,19 @@ def read_single_int():
     return int(sys.stdin.readline().strip())
 
 
+def get_sequence(n, ll):
+    for l in range(ll, 101):
+        if (n/l + 1/2 -l/2).is_integer():
+            s = n//l - (l-1)//2
+            if s < 0:
+                continue
+            ns = [x for x in range(s, s+l)]
+            return ns
+        elif (n/l + 1/2 -l/2) <= 0:
+            return [-1]
+    return [-1]
+
+
 if __name__ == '__main__':
-    
+    n, l = read_list_int()
+    print(' '.join(map(str, get_sequence(n, l))))
