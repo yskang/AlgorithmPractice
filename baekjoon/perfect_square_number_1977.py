49869b98c94024e064f79752ff707daa0e70401e
Yongsung Kang <yongsung.kang@gmail.com>
2018-05-28 15:34:36 +0900

    perfect square number - 1977, baekjoon

---
+++ baekjoon/perfect_square_number_1977.py
@@ -0,0 +1,18 @@
+# Title: 완전제곱수
+# Link: https://www.acmicpc.net/problem/1977
+
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+
+def read_list_int():
+    return list(map(int, sys.stdin.readline().strip().split(' ')))
+
+
+def read_single_int():
+    return int(sys.stdin.readline().strip())
+
+
+if __name__ == '__main__':
+    
