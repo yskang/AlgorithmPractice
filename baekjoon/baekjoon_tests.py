baa0848312477a6751aaeabb809cc9323f0d5753
Yongsung Kang <yongsung.kang@gmail.com>
2018-05-28 15:36:17 +0900

    perfect square number - 1977, baekjoon

--- baekjoon/baekjoon_tests.py
+++ baekjoon/baekjoon_tests.py
@@ -1,9 +1,74 @@
+import random
 import unittest
 
+import baekjoon.perfect_square_number_1977
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class PerfectSquareNumber_1977(unittest.TestCase):
+    def test_1(self):
+        M = 60
+        N = 100
+        res = '245\n64'
+        self.assertEqual(baekjoon.perfect_square_number_1977.perfect_square_number(M, N), res)
+
+    def test_no_answer(self):
+        M = 2
+        N = 2
+        res = '-1'
+        self.assertEqual(baekjoon.perfect_square_number_1977.perfect_square_number(M, N), res)
+
+    def test_min_max(self):
+        M = 1
+        N = 10000
+        s = 0
+        m = 9999999
+        for i in range(1, 1000000):
+            i_s = i * i
+            if i_s > N:
+                break
+            s += i_s
+            m = min(i_s, m)
+
+        res = '{}\n{}'.format(s, m)
+        self.assertEqual(baekjoon.perfect_square_number_1977.perfect_square_number(M, N), res)
+
+    def test_1_diff_front(self):
+        M = 1
+        N = 2
+        s = 1
+        m = 1
+        res = '{}\n{}'.format(s, m)
+        self.assertEqual(baekjoon.perfect_square_number_1977.perfect_square_number(M, N), res)
+
+    def test_1_diff_last(self):
+        M = 9999
+        N = 10000
+        s = 10000
+        m = 10000
+        res = '{}\n{}'.format(s, m)
+        self.assertEqual(baekjoon.perfect_square_number_1977.perfect_square_number(M, N), res)
+
+    def test_0_diff(self):
+        M = 1
+        N = 1
+        s = 1
+        m = 1
+        res = '{}\n{}'.format(s, m)
+        self.assertEqual(baekjoon.perfect_square_number_1977.perfect_square_number(M, N), res)
+
+    def test_random(self):
+        count = 1000
+        while count > 0:
+            count -= 1
+            M = random.randint(1, 10000)
+            N = M + random.randint(0, 10000-M)
+            s = 1
+            m = 1
+            res = '{}\n{}'.format(s, m)
+            try:
+                baekjoon.perfect_square_number_1977.perfect_square_number(M, N)
+            except:
+                print('fail on {}, {}'.format(M, N))
 
 
 if __name__ == '__main__':
--- baekjoon/perfect_square_number_1977.py
+++ baekjoon/perfect_square_number_1977.py
@@ -1,6 +1,6 @@
 # Title: 완전제곱수
 # Link: https://www.acmicpc.net/problem/1977
-
+import math
 import sys
 
 sys.setrecursionlimit(10 ** 6)
@@ -14,5 +14,20 @@ def read_single_int():
     return int(sys.stdin.readline().strip())
 
 
+def perfect_square_number(M, N):
+    nums = []
+    for n in range(M, N+1):
+        if math.sqrt(n).is_integer():
+            nums.append(n)
+    if nums:
+        sum_of_them = sum(nums)
+        minimum_number = min(nums)
+        return '\n'.join(map(str, [sum_of_them, minimum_number]))
+    else:
+        return '-1'
+
+
 if __name__ == '__main__':
-    
+    M = read_single_int()
+    N = read_single_int()
+    print(perfect_square_number(M, N))
