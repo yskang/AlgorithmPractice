4daae5f34d457202d000c3dac2f46e7f53e48c47
Yongsung Kang <yongsung.kang@gmail.com>
2018-06-21 17:52:00 +0900

    n n square number - 1016, baekjoon

--- baekjoon/n_n_square_number_1016.py
+++ baekjoon/n_n_square_number_1016.py
@@ -14,26 +14,22 @@ def read_single_int():
     return int(sys.stdin.readline().strip())
 
 
-def is_nn(num):
-    for n in range(2, int(math.sqrt(num))+1):
-        if num % n**2 == 0:
-            return True
-    return False
-
-
 def num_nn_square(minimum, maximum):
-    numbers = [1 for _ in range(minimum, maximum+1)]
+    numbers = [True for _ in range(maximum-minimum+1)]
+    count = len(numbers)
     n = 2
-    while True:
-        nn = n**2
-        d = 1
-        while True:
-            nn *= d
-            numbers[nn] = 0
-            d += 1
-
-
-    return sum(numbers)
+    while n*n <= maximum:
+        n_square = n*n
+        i = minimum // n_square
+        while n_square*i <= maximum:
+            index = n_square*i-minimum
+            if index >= 0:
+                if numbers[index]:
+                    count -= 1
+                    numbers[index] = False
+            i += 1
+        n += 1
+    return count
 
 
 if __name__ == '__main__':
