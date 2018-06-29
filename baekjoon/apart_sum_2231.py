13fb74e73260bdfc8865cbd1cad724f17b127c5f
Yongsung Kang <yongsung.kang@gmail.com>
2018-06-29 15:41:06 +0900

    apart sum - 2231, baekjoon

---
+++ baekjoon/apart_sum_2231.py
@@ -0,0 +1,29 @@
+# Title: 분해합
+# Link: https://www.acmicpc.net/problem/2231
+
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+
+def read_list_int():
+    return list(map(int, sys.stdin.readline().strip().split(' ')))
+
+
+def read_single_int():
+    return int(sys.stdin.readline().strip())
+
+
+def get_sum(n):
+    for creator in range(1, n):
+        s = creator
+        for c in str(creator):
+            s += int(c)
+        if n == s:
+            return creator
+    return 0
+
+
+if __name__ == '__main__':
+    n = read_single_int()
+    print(get_sum(n))
