801a9cf5232546f58abd3bb6469e3a15013fa040
강용성 <yskang@gang-yongseong-ui-MacBookPro.local>
2018-10-10 18:23:49 +0900

    longest increasing subsequence - 11053, baekjoon

---
+++ baekjoon/longest_increasing_subsequence_11053.py
@@ -0,0 +1,27 @@
+# Title: 가장 긴 증가하는 부분 수열
+# Link: https://www.acmicpc.net/problem/11053
+
+import sys
+
+
+sys.setrecursionlimit(10 ** 6)
+
+
+read_single_int = lambda: int(sys.stdin.readline().strip())
+read_list_int = lambda: list(map(int, sys.stdin.readline().strip().split(' ')))
+
+
+def get_LIS_length(nums: list, length: int):
+    lis_list = [1 for _ in range(length)]
+    for i in range(length):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                lis_list[i] = max(lis_list[j] + 1, lis_list[i])
+
+    return max(lis_list)
+
+
+if __name__ == '__main__':
+    l = read_single_int()
+    a = read_list_int()
+    print(get_LIS_length(a, l))
