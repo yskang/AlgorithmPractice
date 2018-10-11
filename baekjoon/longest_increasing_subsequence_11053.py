e5018fd690f5379c04cb9b921fe890b7aa199abb
Yongsung Kang <yongsung.kang@gmail.com>
2018-10-11 22:06:51 +0900

    longest increasing subsequence - 11053, baekjoon

--- baekjoon/longest_increasing_subsequence_11053.py
+++ baekjoon/longest_increasing_subsequence_11053.py
@@ -2,6 +2,7 @@
 # Link: https://www.acmicpc.net/problem/11053
 
 import sys
+import bisect
 
 
 sys.setrecursionlimit(10 ** 6)
@@ -11,7 +12,7 @@ read_single_int = lambda: int(sys.stdin.readline().strip())
 read_list_int = lambda: list(map(int, sys.stdin.readline().strip().split(' ')))
 
 
-def get_LIS_length(nums: list, length: int):
+def get_LIS_length_N2(nums: list, length: int):
     lis_list = [1 for _ in range(length)]
     for i in range(length):
         for j in range(i):
@@ -20,9 +21,26 @@ def get_LIS_length(nums: list, length: int):
 
     return max(lis_list)
 
-# todo: use dp
 
-if __name__ == '__main__':
+def get_LIS_length_NlogN(nums: list, length: int):
+    lis_list = [0]
+
+    for num in nums:
+        i = bisect.bisect_left(lis_list, num)
+        if len(lis_list) == i:
+            lis_list.append(num)
+        else:
+            lis_list[i] = num
+
+    return len(lis_list)-1
+
+
+def main():
     l = read_single_int()
     a = read_list_int()
-    print(get_LIS_length(a, l))
+    # print(get_LIS_length_N2(a, l))
+    print(get_LIS_length_NlogN(a, l))
+
+
+if __name__ == '__main__':
+    main()
