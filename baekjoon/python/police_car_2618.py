e88fad3f2a30c4bbedb811894bb15dc3bd56e94d
Yongsung Kang <yongsung.kang@gmail.com>
2018-10-31 10:57:34 +0900

    use global keyword

--- baekjoon/python/police_car_2618.py
+++ baekjoon/python/police_car_2618.py
@@ -11,62 +11,59 @@ read_list_int = lambda: list(map(int, sys.stdin.readline().strip().split(' ')))
 
 INF = 999999999999999
 
-class Global:
-    def __init__(self):
-        self.dp = []
-        self.events = []
-        self.event_size = 0
-        self.width = 0
-        self.dp_a = []
-        self.dp_b = []
+dp = []
+events = []
+event_size = 0
+width = 0
+dp_a = []
+dp_b = []
 
 
 def distance(a: tuple, b: tuple):
     return abs(a[0]-b[0]) + abs(a[1]-b[1])
 
 
-def min_distance(x: int, y: int, g: Global):
-    if x == g.event_size or y == g.event_size:
+def min_distance(x: int, y: int):
+    global dp, dp_a, dp_b, width, events
+    if x == event_size or y == event_size:
         return 0
-    if g.dp[x][y] != -1:
-        return g.dp[x][y]
-    first = g.dp_a[x][y] = min_distance(max(x, y)+1, y, g) + distance(g.events[max(x, y)+1], (1, 1) if x == 0 else g.events[x])
-    second = g.dp_b[x][y] =  min_distance(x, max(x, y)+1, g) + distance(g.events[max(x, y)+1], (g.width, g.width) if y == 0 else g.events[y])
-    g.dp[x][y] = min(first, second)
-    return g.dp[x][y]
+    if dp[x][y] != -1:
+        return dp[x][y]
+    first = dp_a[x][y] = min_distance(max(x, y)+1, y) + distance(events[max(x, y)+1], (1, 1) if x == 0 else events[x])
+    second = dp_b[x][y] =  min_distance(x, max(x, y)+1) + distance(events[max(x, y)+1], (width, width) if y == 0 else events[y])
+    dp[x][y] = min(first, second)
+    return dp[x][y]
 
 
-def print_route(x: int, y: int, g: Global):
-    if x == g.event_size or y == g.event_size:
+def print_route(x: int, y: int):
+    global event_size, dp_a, dp_b
+    if x == event_size or y == event_size:
         return
-    if g.dp_a[x][y] < g.dp_b[x][y]:
+    if dp_a[x][y] < dp_b[x][y]:
         print('1')
-        print_route(max(x, y)+1, y, g)
+        print_route(max(x, y)+1, y)
     else:
         print('2')
-        print_route(x, max(x, y)+1, g)
+        print_route(x, max(x, y)+1)
 
 
-def solution(g: Global):
-    g.dp = [[-1 for _ in range(g.event_size+1)] for _ in range(g.event_size+1)]
-    g.dp_a = [[-1 for _ in range(g.event_size+1)] for _ in range(g.event_size+1)]
-    g.dp_b = [[-1 for _ in range(g.event_size+1)] for _ in range(g.event_size+1)]
-    g.events = [0] + g.events
-    print(min_distance(0, 0, g))
-    print_route(0, 0, g)
+def solution():
+    global dp, dp_a, dp_b, events, event_size
+    dp = [[-1 for _ in range(event_size+1)] for _ in range(event_size+1)]
+    dp_a = [[-1 for _ in range(event_size+1)] for _ in range(event_size+1)]
+    dp_b = [[-1 for _ in range(event_size+1)] for _ in range(event_size+1)]
+    events = [0] + events
+    print(min_distance(0, 0))
+    print_route(0, 0)
 
 
 def main():
-    g = Global()
-    n = read_single_int()
-    w = read_single_int()
-    for _ in range(w):
-        g.events.append(read_list_int())
-
-    g.event_size = w
-    g.width = n
-
-    solution(g)
+    global events, event_size, width
+    width = read_single_int()
+    event_size = read_single_int()
+    for _ in range(event_size):
+        events.append(read_list_int())
+    solution()
 
 
 if __name__ == '__main__':
