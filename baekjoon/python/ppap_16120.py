d2862fe5a6aafd64581eadde4d66578f10178dae
Yongsung Kang <yongsung.kang@gmail.com>
2018-12-25 21:24:31 +0900

    PPAP - 16120, baekjoon

--- baekjoon/python/ppap_16120.py
+++ baekjoon/python/ppap_16120.py
@@ -14,29 +14,19 @@ def solution(s: list):
     stack = []
     for c in s:
         stack.append(c)
-        if stack[len(stack)-4 if len(stack)-4 >= 0 else 0:len(stack)] == ['P', 'P', 'A', 'P']:
+        if stack[len(stack)-4 if len(stack)-4 >= 0 else len(stack):len(stack)] == ['P', 'P', 'A', 'P']:
             stack.pop()
             stack.pop()
             stack.pop()
 
-    stack = ''.join(stack)
-    if stack.count('A') == 0:
+    if stack == ['P']:
         return 'PPAP'
     return 'NP'
 
 
 def main():
-    # s = read_list_str()
-    while True:
-        s = 'PPAPA'
-        for i in range(50):
-            k = random.randint(0, len(s)-1)
-            if s[k] == 'P':
-                s = s[:k] + 'PPAP' + s[k+1:]
-        
-        if solution(list(s)) == 'PPAP':
-            print(s)
-            break
+    s = read_list_str()
+    print(solution(s))
 
 
 if __name__ == '__main__':
