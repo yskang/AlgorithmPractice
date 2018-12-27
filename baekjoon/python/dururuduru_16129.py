13b4ca3792c21c9de8c6edfd76b24edf7a4b7e90
Yongsung Kang <yongsung.kang@gmail.com>
2018-12-27 15:30:19 +0900

    ...

--- baekjoon/python/dururuduru_16129.py
+++ baekjoon/python/dururuduru_16129.py
@@ -11,63 +11,30 @@ sys.setrecursionlimit(10 ** 6)
 
 read_list_int = lambda: list(map(int, sys.stdin.readline().strip().split(' ')))
 
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+dururuduru = ['r', 'r', 'd', 'r']
 
-class Cell:
-
-    def __init__(self):
-        self.value = ''
-        self.total = 0
-        self.edges = [0, 0, 0, 0] #bottom, top, right, left, in
-
-    def __str__(self):
-        return '[value: {}, total: {}, [{}, {}, {}, {}]]'.format(self.value, self.total, self.top, self.bottom, self.left, self.right)
-
-
-def get_paths(m: list, x: int, y: int, w: int, h: int):
-    duruduru = 'rrdr'
-    directions = [[0, -1], [0, 1], [-1, 0], [1, 0]] # top, bottom, left, right
-    translate = [1, 0, 3, 2]
+def dfs(m: list, x: int, y: int, width: int, height: int, depth: int, ox: int, oy: int):
+    if depth == 4:
+        return 1
     count = 0
-    nexts = set()
-    nexts.add((x, y))
-    new_nexts = set()
-
-    for nc in duruduru:
-        for xx, yy in nexts:
-            for d, direction in enumerate(directions):
-                nx = xx+direction[0]
-                ny = yy+direction[1]
-                if 0 <= nx < w and 0 <= ny < h and m[ny][nx].value == nc:
-                    v = m[yy][xx].total - m[yy][xx].edges[translate[d]]
-                    if v > 0:
-                        m[ny][nx].edges[d] = v
-                        new_nexts.add((nx, ny))
-                        m[yy][xx].edges[translate[d]] = -m[yy][xx].total
-
-        print(new_nexts)
-
-        for xx, yy in new_nexts:
-            m[yy][xx].total = sum(m[yy][xx].edges)
-
-        nexts, new_nexts = new_nexts, nexts
-        new_nexts = set()
-
-    for xx, yy in nexts:
-        count += m[yy][xx].total
-    
-  
-    print(count)
-    print_map(m)
+    for dx, dy in directions:
+        nx, ny = x+dx, y+dy
+        if not(ox == nx and oy == ny) and 0 <= nx < width and 0 <= ny < height and m[ny][nx] == dururuduru[depth]:
+            count += dfs(m, nx, ny, width, height, depth+1, x, y)
     return count
 
 
-def print_map(m: list):
-    for row in m:
-        print(list(map(lambda x: x.total, row)))
+def get_paths(m: list, x: int, y: int, width: int, height: int):
+    count = 0
+    count += dfs(m, x, y, width, height, 0, -1, -1)
+    return count
 
 
 def solution(r: int, c: int):
-    m = [[Cell() for _ in range(c)] for _ in range(r)]
+    m = [['N' for _ in range(c)] for _ in range(r)]
+    ds = []
+
     s = collections.deque()
 
     s.append('d')
@@ -76,18 +43,16 @@ def solution(r: int, c: int):
     s.append('d')
     s.append('r')
 
-    for row in m:
-        for i, _ in enumerate(row):
-            row[i].value = s[0]
+    for y, row in enumerate(m):
+        for x, _ in enumerate(row):
+            row[x] = s[0]
+            if s[0] == 'd':
+                ds.append((x, y))
             s.rotate(-1)
 
     count = 0
-    for y, row in enumerate(m):
-        for x, cell in enumerate(row):
-            if cell.value == 'd':
-                temp = copy.deepcopy(m)
-                temp[y][x].total = 1
-                count += get_paths(temp, x, y, c, r)
+    for x, y in ds:
+        count += dfs(m, x, y, c, r, 0, -1, -1)
 
     return count
 
