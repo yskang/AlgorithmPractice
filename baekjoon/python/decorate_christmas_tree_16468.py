36e598a96b49ed103dfd7b54bc13611ea6f7fec7
Yongsung Kang <yongsung.kang@gmail.com>
2019-03-18 00:40:04 +0900

    change some..

--- baekjoon/python/decorate_christmas_tree_16468.py
+++ baekjoon/python/decorate_christmas_tree_16468.py
@@ -18,18 +18,14 @@ def solution(num_ball: int, height: int):
     fill_table = [[0 for _ in range(301)] for _ in range(301)]
     fill_table[1][1] = 1
 
-    total_balls = 0
     for level in range(2, height+1):
-        total_balls += pow(2, level-1)
-        for ball in range(2, min(total_balls+2, num_ball+1)):
+        for ball in range(2, min(pow(2, level), num_ball+1)):
             s = 0
             ss = 0
-            prev_balls = total_balls-pow(2, level-1)+1
-            start = min(prev_balls, ball-1)
-            end = ball-1-start
-            for i in range(start, end-1, -1):
+            for i in range(min(pow(2, level-1)-1, ball-1), ball-min(pow(2, level-1)-1, ball-1)-2, -1):
                 s += (no_fill_table[level-1][i] * no_fill_table[level-1][ball-1-i])
-                ss += (fill_table[level-1][i] * no_fill_table[level-1][ball-1-i] + no_fill_table[level-1][i] * fill_table[level-1][ball-1-i] - fill_table[level-1][i] * fill_table[level-1][ball-1-i])
+                ss += (fill_table[level-1][i] * no_fill_table[level-1][ball-1-i] + (no_fill_table[level-1][i] - fill_table[level-1][i]) * fill_table[level-1][ball-1-i])
+
 
             no_fill_table[level][ball] = s % MOD
             fill_table[level][ball] = ss % MOD
