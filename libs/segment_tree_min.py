dbda648f3307b8c9c2643c5075714626ef1f2683
Yongsung Kang <yongsung.kang@gmail.com>
2019-05-10 16:56:15 +0900

    segment tree, segment min value

--- libs/segment_tree_min.py
+++ libs/segment_tree_min.py
@@ -1,13 +1,3 @@
-# Title: 최솟값 찾기
-# Link: https://www.acmicpc.net/problem/11003
-
-import sys
-import collections
-
-
-read_list_int = lambda: sys.stdin.readline().strip().split(' ')
-
-
 class Segment_Min_Tree:
     '''
     A Class used to get partial min of an array and update data
@@ -89,34 +79,44 @@ class Segment_Min_Tree:
             return right_index
 
 
-def solution_window(list_length: int, window_width: int, numbers: list):
-    window = collections.deque([])
-    for i, number in enumerate(numbers):
-        num = int(number)
-        while len(window) != 0 and window[0][1] < i-window_width+1:
-            window.popleft()
-        while len(window) != 0 and num <= window[-1][0]:
-            window.pop()
-        window.append((num, i))
-        print(window[0][0], end=' ')
+
+# init segment tree of an array from index start to end.
+# return index of minimum value of array in range from start to end.
+def init_segment_min(array, tree, node, start, end):
+    if start == end:
+        tree[node] = start
+        return tree[node]
+    mid = (start + end) // 2
+    left = init_segment_min(array, tree, node * 2, start, mid)
+    right = init_segment_min(array, tree, node * 2 + 1, mid + 1, end)
+    if array[left] < array[right]:
+        tree[node] = left
+    else:
+        tree[node] = right
+    return tree[node]
 
 
-def solution(n: int, l: int, a: list):
-    a = list(map(int, a))
-    min_tree = Segment_Min_Tree(a)
-    ds = []
-    for end in range(n):
-        ds.append(a[min_tree.min(max(0, end - l + 1), end)])
-    print(' '.join(map(str, ds)))
-
-
-def main():
-    n, l = read_list_int()
-    n, l = int(n), int(l)
-    a = read_list_int()
-    # solution_window(int(n), int(l), a)
-    solution(n, l, a)
+def find_min(array, tree, node, start, end, left, right):
+    if left > end or right < start:
+        return -1
+    if left <= start and end <= right:
+        return tree[node]
+    left_index = find_min(array, tree, node*2, start, (start+end)//2, left, right)
+    right_index = find_min(array, tree, node*2+1, (start+end)//2+1, end, left, right)
+    if left_index == -1 and right_index == -1:
+        return -1
+    elif left_index == -1:
+        return right_index
+    elif right_index == -1:
+        return left_index
+    else:
+        if array[left_index] < array[right_index]:
+            return left_index
+        return right_index
 
 
 if __name__ == '__main__':
-    main()
+    a = [3, -1, 6, 7, 2, 9, 4, 5, 2, 8, 1, 5]
+
+    segment = Segment_Min_Tree(a)
+    print(segment.min(0, 11))
