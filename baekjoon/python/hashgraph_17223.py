508ba6854ac6fc2c42ba93fddd20065479b9a38c
Yongsung Kang <yongsung.kang@gmail.com>
2019-06-10 18:22:00 +0900

    hashgraph, baekjoon 17223

--- baekjoon/python/hashgraph_17223.py
+++ baekjoon/python/hashgraph_17223.py
@@ -6,25 +6,18 @@ import random
 
 sys.setrecursionlimit(10 ** 6)
 
+read_list_int = lambda: list(map(int, sys.stdin.readline().replace('\r', '').replace('\n', '').strip().split(' ')))
 
-def safe_int(s: str):
-    return int(s.strip())
+def solution(n: int, m: int, transmit: list, want: list):
+    events = [[(i, 0)] for i in range(n)]
+    for send, receive in transmit:
+        events[receive].append((send, len(events[send])-1))
 
-read_list_int = lambda: list(map(safe_int, sys.stdin.readline().strip().split(' ')))
-
-
-def solution(n: int, m: int, events: list, want: list):
-    users = [[(i, 0)] for i in range(n)]
-    for f, t in events:
-        users[t].append((f, len(users[f])-1))
-
-    a, a_event, b, b_event = want
-
-    prev_user, prev_event = a, a_event
+    prev_user, prev_event, target_user, target_event = want
 
     while True:
-        next_user, next_event = users[prev_user][prev_event]
-        if (next_user, next_event) == (b, b_event):
+        next_user, next_event = events[prev_user][prev_event]
+        if (next_user, next_event) == (target_user, target_event):
             return 1
         if (next_user, next_event) == (prev_user, prev_event):
             return 0
@@ -35,32 +28,11 @@ def solution(n: int, m: int, events: list, want: list):
 
 def main():
     n, m = read_list_int()
-    events = []
+    transmit = []
     for _ in range(m):
-        events.append(read_list_int())
+        transmit.append(read_list_int())
     want = read_list_int()
-    print(solution(n, m, events, want))
-
-
-def test():
-    while True:
-        n = random.randint(2, 100)
-        m = random.randint(1, 10000)
-        events = []
-        users = [[0] for _ in range(n)]
-        for _ in range(m):
-            f = random.randint(0, n-1)
-            t = random.randint(0, n-1)
-            users[t].append(0)
-            events.append([f, t])
-        a = random.randint(0, n-1)
-        a_event = random.randint(0, len(users[a])-1)
-        b = random.randint(0, n-1)
-        b_event = random.randint(0, len(users[b])-1)
-        want = [a, a_event, b, b_event]
-
-        print(n, m, want)
-        print(solution(n, m, events, want))
+    print(solution(n, m, transmit, want))
 
 
 if __name__ == '__main__':
