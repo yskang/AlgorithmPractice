965ee31a1307fecf7dfe1e1ca02dd7211ed10563
Yongsung Kang <yongsung.kang@gmail.com>
2019-06-20 13:46:12 +0900

    cipher code, baekjoon 2011

--- baekjoon/python/cipher_code_2011.py
+++ baekjoon/python/cipher_code_2011.py
@@ -2,6 +2,7 @@
 # Link: https://www.acmicpc.net/problem/2011
 
 import sys
+import random
 
 
 sys.setrecursionlimit(10 ** 6)
@@ -9,24 +10,77 @@ sys.setrecursionlimit(10 ** 6)
 read_single_str = lambda: sys.stdin.readline().strip()
 
 
-def solution(code: str):
-    if len(code) == 1:
-        if code == '0':
-            return 0
+DIV = 1000000
+
+def is_valid(first: str, second=None):
+    if second==None:
+        first = int(first)
+        if 1 <= first <= 26:
+            return True
+        return False
+    else:
+        if first == '0':
+            return False
+        elif 1 <= int(first+second) <= 26:
+            return True
         else:
+            return False
+
+
+def count(code: str, i: int, cache: list, is_valid_pw: list):
+    if not is_valid_pw[0]:
+        return 0
+    if i < 0:
+        return 0
+    if cache[i] != -1:
+        return cache[i]
+    if i == 0:
+        cache[i] = 1
+        return 1
+    elif i == 1:
+        if int(code[:2]) == 10 or int(code[:2]) == 20:
             return 1
+        elif 10 < int(code[:2]) <= 26:
+            cache[i] = 2
+            return 2
+        if int(code[1]) == 0 and int(code[0]) >= 3:
+            is_valid_pw[0] = False
+            return 0
+        cache[i] = 1
+        return 1
 
-    d = [0 for _ in range(len(code)+2)]
-    d[1] = 1
-    d[2] = 2 if int(code[0:2]) <= 26 and int(code[1:2]) != 0 else 1
+    ans = 0
+    # first_valid = is_valid(code[i-1])
+    second_valid = is_valid(code[i])
+    two_valid = is_valid(code[i-1], code[i])
 
-    for i in range(3, len(code)+1):
-        if int(code[i-1:i]) != 0:
-            d[i] += d[i-1]
-        if int(code[i-2:i]) <= 26 and int(code[i-2:i-1]) != 0:
-            d[i] += d[i-2]
+    if not second_valid:
+        if int(code[i-1]) >= 3 or int(code[i-1]) == 0:
+            is_valid_pw[0] = False
+            return 0
 
-    return d[len(code)] % 1000000
+    if second_valid and two_valid:
+        ans = count(code, i-1, cache, is_valid_pw) + count(code, i-2, cache, is_valid_pw)
+    elif not second_valid and two_valid:
+        ans = count(code, i-2, cache, is_valid_pw)
+    elif second_valid and not two_valid:
+        ans = count(code, i-1, cache, is_valid_pw)
+
+    cache[i] = ans
+    return ans
+
+
+def solution(code: str):
+    if code[0] == '0':
+        return 0
+    is_valid_pw = [True]
+    cache = [-1 for _ in range(len(code)+1)]
+    ans = count(code, len(code)-1, cache, is_valid_pw)
+
+    if not is_valid_pw[0]:
+        return 0
+
+    return ans % DIV
 
 
 def main():
