85a4a95b7bf72e4c66c54745e4d3a3471eefe56a
Yongsung Kang <yongsung.kang@gmail.com>
2019-07-26 13:19:12 +0900

    cat, baekjoon 10171

---
+++ baekjoon/python/cat_10171.py
@@ -0,0 +1,22 @@
+# Title: 고양이
+# Link: https://www.acmicpc.net/problem/10171
+
+import sys
+
+
+sys.setrecursionlimit(10 ** 6)
+
+
+def solution():
+    print('\\    /\\')
+    print(' )  ( \')')
+    print('(  /  )')
+    print(' \\(__)|')
+
+
+def main():
+    solution()
+
+
+if __name__ == '__main__':
+    main()
