8e7edaf1d1795bdeace6ded299df526adbcdacf8
Yongsung Kang <yongsung.kang@gmail.com>
2021-05-25 18:34:30 +0900

    gahee, baekjoon

--- baekjoon/rust/dont_slepp_in_there_gahee.rs
+++ baekjoon/rust/dont_slepp_in_there_gahee.rs
@@ -1,20 +1,20 @@
 use std::io::{BufRead};
 
 fn main() {
-    let mut r: i64;
-    let mut c: i64;
-    let mut rg: i64;
-    let mut cg: i64;
-    let mut rp: i64;
-    let mut cp: i64;
+    let mut r: usize;
+    let mut c: usize;
+    let mut rg: usize;
+    let mut cg: usize;
+    let mut rp: usize;
+    let mut cp: usize;
 
     let mut line = String::new();
     std::io::stdin().read_line(&mut line);
-    let mut line_in: Vec::<i64> = line
+    let mut line_in: Vec::<usize> = line
                                     .trim()
                                     .split_whitespace()
                                     .into_iter()
-                                    .map(|x| x.parse::<i64>().unwrap())
+                                    .map(|x| x.parse::<usize>().unwrap())
                                     .collect();
     r = line_in[0];
     c = line_in[1];
@@ -25,13 +25,14 @@ fn main() {
                 .trim()
                 .split_whitespace()
                 .into_iter()
-                .map(|x| x.parse::<i64>().unwrap())
+                .map(|x| x.parse::<usize>().unwrap())
                 .collect();
     rg = line_in[0];
     cg = line_in[1];
     rp = line_in[2];
     cp = line_in[3];
     
+    let mut count_p = 0;
     let mut lines = String::new();
     for i in 0..r {
         std::io::stdin().read_line(&mut lines);
@@ -39,14 +40,14 @@ fn main() {
                 .trim()
                 .parse::<String>()
                 .unwrap();
-        println!("{}", lines);
+        count_p = count_p + lines.matches("P").count();
         lines.clear();
     }
 
-    let mut v: Vec<i64> = vec![10, 20, 30, 40, -2, 3];
-    println!("{:?}", v);
-    v.sort();
-    v.reverse();
-    println!("{:?}", v);
-
+    if count_p == (cp*rp).into() {
+        println!("0");
+    } else {
+        println!("1");
+    }
+ 
 }
