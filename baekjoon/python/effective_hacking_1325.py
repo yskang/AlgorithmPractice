54bf5de96cf177c3c600643f98dec826bedc4fd3
Yongsung Kang <yongsung.kang@gmail.com>
2021-06-17 18:50:04 +0900

    wip

--- baekjoon/python/effective_hacking_1325.py
+++ baekjoon/python/effective_hacking_1325.py
@@ -1,117 +1,129 @@
-# Title: 효율적인 해킹
+# Title: 효율적인 홍보
 # Link: https://www.acmicpc.net/problem/1325
 
 import sys
-import copy
+from typing import DefaultDict, Set
 from collections import defaultdict
 
-sys.setrecursionlimit(10 ** 6)
+
+read_list_int = lambda: list(map(int, sys.stdin.readline().strip().split(' ')))
 
 
-def read_list_int():
-    return list(map(int, sys.stdin.readline().strip().split(' ')))
-
-
-def read_single_int():
-    return int(sys.stdin.readline().strip())
-
-
-def dfs(graph: list, n: int, visited: list, stack: list):
-    if visited[n]:
-        return
-    visited[n] = True
-
-    for ch in graph[n]:
-        dfs(graph, ch, visited, stack)
-
-    stack.append(n)
-
-
-def maximum_node(N: int, graph: list, graph_reverse: list, start_nodes: list):
+def get_scc_iter(vertices, edges):
+    identified = set()
     stack = []
-    visited = [False for _ in range(N+1)]
+    index = {}
+    boundaries = []
 
-    for node in range(1, N+1):
-        if start_nodes[node]:
-            dfs(graph, node, visited, stack)
-            if len(stack) == N:
-                break
+    for v in vertices:
+        if v not in index:
+            to_do = [('VISIT', v)]
+            while to_do:
+                operation_type, v = to_do.pop()
+                if operation_type == 'VISIT':
+                    index[v] = len(stack)
+                    stack.append(v)
+                    boundaries.append(index[v])
+                    to_do.append(('POSTVISIT', v))
+                    to_do.extend(
+                        reversed([('VISITEDGE', w) for w in edges[v]]))
+                elif operation_type == 'VISITEDGE':
+                    if v not in index:
+                        to_do.append(('VISIT', v))
+                    elif v not in identified:
+                        while index[v] < boundaries[-1]:
+                            boundaries.pop()
+                else:
+                    if boundaries[-1] == index[v]:
+                        boundaries.pop()
+                        scc = set(stack[index[v]:])
+                        del stack[index[v]:]
+                        identified.update(scc)
+                        yield scc
 
-    if len(stack) != N:
-        for node, is_visit in enumerate(visited[1:], 1):
-            if not is_visit:
-                dfs(graph, node, visited, stack)
-            if len(stack) == N:
-                break
 
-    visited = [False for _ in range(N+1)]
-    map_table = [i for i in range(N+1)]
-    count_table = [[i] for i in range(N+1)]
 
-    for node in reversed(stack):
-        temp = []
-        dfs(graph_reverse, node, visited, temp)
-        sorted_temp = sorted(temp)
-        if temp:
-            count_table[sorted_temp[0]] = sorted_temp
-            for node in temp:
-                map_table[node] = sorted_temp[0]
+def solution(n: int, pairs: DefaultDict, starts: Set, pairs_rev: DefaultDict) -> str:
+    counts = defaultdict(lambda: set())
+    nodes = [i for i in range(1, n+1)]
+    new_node = n+1
+    scc_map = defaultdict(lambda:None)
 
-    start_nodes = [True for _ in range(N+1)]
-    for i, nodes in enumerate(graph[1:], 1):
-        childs = set()
-        for node in nodes:
-            childs.add(map_table[node])
-        if map_table[i] == i:
-            graph[i] = list(childs)
+    for scc in get_scc_iter(nodes, pairs):
+        if len(scc) > 1:
+            scc_map[new_node] = set(scc)
+            outs = set()
+            ins = set()
+            for node in scc:
+                outs.update(pairs[node])
+                ins.update(pairs_rev[node])
+            outs = outs.difference(scc)
+            ins = ins.difference(scc)
+            pairs[new_node].update(outs)
+
+            for node in ins:
+                pairs[node] = pairs[node].difference(scc)
+                pairs[node].add(new_node)
+
+            for node in outs:
+                pairs_rev[node] = pairs_rev[node].difference(scc)
+                pairs_rev[node].add(new_node)
+
+            counts[new_node] = scc
+            if len(ins) == 0:
+                starts.add(new_node)
+            new_node += 1
+
+    temp = set()
+    while True:
+        for node in starts:
+            for manto in pairs[node]:
+                if len(counts[node]) == 0:
+                    counts[node].add(node)
+                if len(counts[manto]) == 0:
+                    counts[manto].add(manto)
+                counts[manto].update(counts[node])
+                pairs_rev[manto].discard(node)
+                if len(pairs_rev[manto]) == 0:
+                    temp.add(manto)
+        starts, temp = temp, set()
+        if len(starts) == 0:
+            break
+
+    res = []
+
+    counts_lens = defaultdict(lambda: 0)
+    for node in counts:
+        counts_lens[node] = len(counts[node])
+
+    max_count = max(counts_lens.values())
+    for node in counts_lens:
+        if counts_lens[node] == max_count:
+            res.append(node)
+    ans  = set()
+    for r in res:
+        if r in scc_map:
+            for c in scc_map[r]:
+                ans.add(c)
         else:
-            new_i = map_table[i]
-            l = set(graph[new_i]).union(childs)
-            l.discard(new_i)
-            graph[new_i] = list(l)
-            graph[i] = []
+            ans.add(r)
+    ans = sorted(ans)
 
-    for n, childs in enumerate(graph[1:], 1):
-        for child in childs:
-            start_nodes[child] = False
-
-    max_nodes = []
-    max_count = 0
-    for n in range(1, N+1):
-        if start_nodes[n] == True:
-            visits = []
-            has_visit = defaultdict(lambda: False)
-            dfs(graph, n, has_visit, visits)
-            count = 0
-            for visit in visits:
-                count += len(count_table[visit])
-
-            if count > max_count:
-                max_count = count
-                max_nodes = [n]
-            elif count == max_count:
-                max_nodes.append(n)
-
-    results = []
-    for node in max_nodes:
-        results += count_table[node]
-
-    return ' '.join(map(str, sorted(results)))
+    return ' '.join(map(str, ans))
 
 
 def main():
-    N, M = read_list_int()
-    graph = [[] for _ in range(N+1)]
-    graph_reverse = [[] for _ in range(N+1)]
-    start_nodes = [True for i in range(N+1)]
+    n, m = read_list_int()
+    pairs = defaultdict(lambda: set())
+    pairs_rev = defaultdict(lambda: set())
+    starts = set([i for i in range(1, n+1)])
 
-    for _ in range(M):
-        a, b = read_list_int()
-        graph[b].append(a)
-        graph_reverse[a].append(b)
-        start_nodes[a] = False
-
-    print(maximum_node(N, graph, graph_reverse, start_nodes))
+    for _ in range(m):
+        manti, manto = read_list_int()
+        pairs[manti].add(manto)
+        pairs_rev[manto].add(manti)
+        starts.discard(manto)
+    print(solution(n, pairs, starts, pairs_rev))
 
 
 if __name__ == '__main__':
