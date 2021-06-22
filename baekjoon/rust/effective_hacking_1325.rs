0091ca58045867dcf49f02ca01a17ae9c119d425
Yongsung Kang <yongsung.kang@gmail.com>
2021-06-22 19:54:31 +0900

    wip

--- baekjoon/rust/effective_hacking_1325.rs
+++ baekjoon/rust/effective_hacking_1325.rs
@@ -1,10 +1,10 @@
 // Title: 효율적인 해킹
 // Link: https://www.acmicpc.net/problem/1325
 
-use std::collections::HashMap;
+use std::cmp::max;
+use std::collections::{HashMap, HashSet};
 use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
 use std::mem::swap;
-use std::cmp::max;
 
 macro_rules! read_to_tuple {
     ($buffer:expr, $($t:ty),+) => {{
@@ -30,13 +30,71 @@ macro_rules! read_to_vector {
     }};
 }
 
-fn get_scc_iter(vertices: &mut Vec<usize>, edges: &mut HashMap<usize, HashSet<usize>) -> 
+fn get_scc_iter(
+    vertices: &mut Vec<usize>,
+    edges: &mut HashMap<usize, HashSet<usize>>,
+) -> Vec<HashSet<usize>> {
+    let mut sccs: Vec<HashSet<usize>> = Vec::new();
+    let mut identified: HashSet<usize> = HashSet::new();
+    let mut stack: Vec<usize> = Vec::new();
+    let mut index: HashMap<usize, usize> = HashMap::new();
+    let mut boundaries: Vec<usize> = Vec::new();
 
+    for v in vertices {
+        if !index.contains_key(&v) {
+            let mut to_do: Vec<(&str, usize)> = vec![("VISIT", *v)];
+            while to_do.len() > 0 {
+                let (operation_type, vv) = to_do.pop().unwrap();
+                if operation_type == "VISIT" {
+                    index.insert(vv, stack.len());
+                    stack.push(vv);
+                    boundaries.push(index[&vv]);
+                    to_do.push(("POSTVISIT", vv));
+                    let rev: Vec<(&str, usize)> = Vec::new();
+                    for w in edges[&vv] {
+                        rev.push(("VISITEDGE", w))
+                    }
+                    rev.reverse();
+                    for tup in rev {
+                        to_do.push(tup);
+                    }
+                } else if operation_type == "VISITEDGE" {
+                    if !index.contains_key(&vv) {
+                        to_do.push(("VISIT", vv))
+                    } else if !identified.contains(&vv) {
+                        while index[&vv] < *boundaries.last().unwrap() {
+                            boundaries.pop();
+                        }
+                    }
+                } else {
+                    if *boundaries.last().unwrap() == index[&vv] {
+                        boundaries.pop();
+                        let mut scc: HashSet<usize> = HashSet::new();
+                        for t in stack[index[&vv]..].iter_mut() {
+                            scc.insert(*t);
+                        }
+                        for s in scc {
+                            identified.insert(s);
+                        }
+                        sccs.push(scc);
+                        stack.resize(index[&vv], 0);
+                    }
+                }
+            }
+        }
+    }
+    sccs
+}
 
-fn solution(n: usize, pairs: HashMap<usize, HashSet<usize>>, starts: HashSet<usize>, pairs_rev: HashMap<usize, HashSet<usize>>) -> String {
+fn solution(
+    n: usize,
+    pairs: HashMap<usize, HashSet<usize>>,
+    starts: HashSet<usize>,
+    pairs_rev: HashMap<usize, HashSet<usize>>,
+) -> String {
     let mut counts: HashMap<usize, HashSet<usize>> = HashMap::new();
-    let mut nodes = (1..n+1).collect::<Vec<usize>>();
-    let mut new_node = n+1;
+    let mut nodes = (1..n + 1).collect::<Vec<usize>>();
+    let mut new_node = n + 1;
     let mut scc_map: HashMap<usize, HashSet<usize>> = HashMap::new();
 
     for scc in get_scc_iter(&mut nodes, &mut pairs) {
@@ -45,26 +103,32 @@ fn solution(n: usize, pairs: HashMap<usize, HashSet<usize>>, starts: HashSet<usi
             let mut outs: HashSet<usize> = HashSet::new();
             let mut ins: HashSet<usize> = HashSet::new();
             for node in scc {
-                pairs.get(node).unwrap().for_each(|x| outs.push(x));
-                pairs_rev.get(node).unwrap().for_each(|x| ins.push(x));
+                pairs.get(&node).unwrap().iter().for_each(|x| {
+                    outs.insert(*x);
+                });
+                pairs_rev.get(&node).unwrap().iter().for_each(|x| {
+                    ins.insert(*x);
+                });
             }
             outs.retain(|x| !scc.contains(x));
-            int.retain(|x| !scc.contains(x));
-            outs.iter().for_each(|x| pairs.get(new_node).unwrap().push(x));
+            ins.retain(|x| !scc.contains(x));
+            outs.iter().for_each(|x| {
+                pairs.get(&new_node).unwrap().insert(*x);
+            });
 
             for node in ins {
-                pairs.get(node).unwrap().retain(|x| !scc.contains(x));
-                pairs.get(node).unwrap().push(new_node);
+                pairs.get(&node).unwrap().retain(|x| !scc.contains(x));
+                pairs.get(&node).unwrap().insert(new_node);
             }
 
             for node in outs {
-                pairs_rev.get(node).unwrap().retain(|x| !scc.contains(x));
-                pairs_rev.get(node).unwrap().push(new_node);
+                pairs_rev.get(&node).unwrap().retain(|x| !scc.contains(x));
+                pairs_rev.get(&node).unwrap().insert(new_node);
             }
 
             counts.insert(new_node, scc);
             if ins.len() == 0 {
-                starts.push(new_node);
+                starts.insert(new_node);
             }
             new_node += 1;
         }
@@ -73,21 +137,23 @@ fn solution(n: usize, pairs: HashMap<usize, HashSet<usize>>, starts: HashSet<usi
     let temp: HashSet<usize> = HashSet::new();
     loop {
         for node in starts {
-            for manto in pairs.get(node).unwrap() {
-                if counts.get(node).unwrap().len() == 0 {
-                    counts.get(node).unwrap().push(node);
+            for manto in pairs.get(&node).unwrap() {
+                if counts.get(&node).unwrap().len() == 0 {
+                    counts.get(&node).unwrap().insert(node);
                 }
                 if counts.get(manto).unwrap().len() == 0 {
-                    counts.get(manto).unwrap().push(manto);
+                    counts.get(manto).unwrap().insert(*manto);
                 }
-                counts.get(node).unwrap().for_each(|x| counts.get(manto).unwrap().push(x));
-                pairs_rev.get(manto).unwrap().remove(node);
+                counts.get(&node).unwrap().iter().for_each(|x| {
+                    counts.get(manto).unwrap().insert(*x);
+                });
+                pairs_rev.get(manto).unwrap().remove(&node);
                 if pairs_rev.get(manto).unwrap().len() == 0 {
-                    temp.push(manto);
+                    temp.insert(*manto);
                 }
             }
         }
-        swap(starts, temp);
+        swap(&mut starts, &mut temp);
         temp.clear();
         if starts.len() == 0 {
             break;
@@ -96,30 +162,34 @@ fn solution(n: usize, pairs: HashMap<usize, HashSet<usize>>, starts: HashSet<usi
 
     let res: Vec<usize> = vec![];
 
-    let counts_lens:HashMap<usize, usize> = HashMap::new();
-    for node in counts {
-        counts_lens.insert(node, counts.get(node).unwrap());
+    let counts_lens: HashMap<usize, usize> = HashMap::new();
+    for (node, _) in counts {
+        counts_lens.insert(node, (counts.get(&node).unwrap()).len());
     }
 
-    let mut max_count = counts_lens.values().iter().max().unwrap();
-    for node in counts_lens {
-        if counts_lens.get(node).unwrap() == max_count {
+    let mut max_count = counts_lens.values().max().unwrap();
+    for (node, _) in counts_lens {
+        if counts_lens.get(&node).unwrap() == max_count {
             res.push(node);
         }
     }
     let ans: HashSet<usize> = HashSet::new();
     for r in res {
-        if scc_map.contains(r) {
-            for c in scc_map.get(r).unwrap() {
-                ans.push(c);
+        if scc_map.contains_key(&r) {
+            for c in scc_map.get(&r).unwrap() {
+                ans.insert(*c);
             }
         } else {
-            ans.push(r);
+            ans.insert(r);
         }
     }
-    ans.sort();
-
-    ans.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" ")
+    let vec_ans: Vec<usize> = ans.into_iter().collect();
+    vec_ans.sort();
+    vec_ans
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
 fn main() {
@@ -129,21 +199,23 @@ fn main() {
     let (n, m) = read_to_tuple!(reader, usize, usize);
     let mut pairs: HashMap<usize, HashSet<usize>> = HashMap::new();
     let mut pairs_rev: HashMap<usize, HashSet<usize>> = HashMap::new();
-    let mut starts: HashSet<usize> = (1..n+1).collect();
+    let mut starts: HashSet<usize> = (1..n + 1).collect();
 
     for _ in 0..m {
         let (manti, manto) = read_to_tuple!(reader, usize, usize);
-        if let Some(mantos) = pairs.get(manti) {
-            mantos.push(manto);
+        if let Some(mantos) = pairs.get(&manti) {
+            mantos.insert(manto);
         } else {
-            pairs[manti].insert(manti, (manto..manto+1).collect());
+            pairs.insert(manti, (manto..manto + 1).collect());
         }
-        if let Some(mantis) = pairs.get(manto) {
-            mantis.push(manti);
+        if let Some(mantis) = pairs.get(&manto) {
+            mantis.insert(manti);
         } else {
-            pairs[manto].insert(manto, (manti..manti+1).collect());
+            pairs.insert(manto, (manti..manti + 1).collect());
         }
-        starts.remove(manto);
+        starts.remove(&manto);
     }
 
-    writeln!(writer, "{}", solution(n, pairs, starts, pairs_rev)).unwrap(); 
+    writeln!(writer, "{}", solution(n, pairs, starts, pairs_rev)).unwrap();
+    writer.flush().unwrap();
+}
--- baekjoon/rust/test.rs
+++ baekjoon/rust/test.rs
@@ -2,84 +2,19 @@ use std::collections::{BTreeMap, HashMap, HashSet};
 use std::mem::swap;
 
 fn main() {
-    let a = (0..10).collect::<Vec<i32>>();
-    let b = vec![false; 10];
-    let c = a.iter().zip(b);
-
-    let mut d = c.collect::<Vec<_>>();
-    d.sort_by_key(|x| -1 * x.0);
-    println!("{:?}", d);
-
-    let word = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
-    println!("{:?}", word);
-    let iter = word[2..4].iter();
-
-    let s = String::from("helloWorld");
-    let vs = s.split("").collect::<Vec<_>>();
-    fun_a(&vs);
-    fun_a(&vs);
-    fun_a(&vs);
-    fun_a(&vs);
-    fun_a(&vs);
-    fun_a(&vs);
-    fun_a(&vs);
-
-    let mut v: Vec<usize> = vec![1; 10];
-    v[2] = 200;
+    let mut v: Vec<(&str, usize)> = Vec::new();
+    v.push(("Hello", 1));
+    v.push(("World", 2));
+    v.push(("Good", 3));
+    v.push(("Morning", 4));
+    v.push(("YSKang", 5));
     println!("{:?}", v);
 
-    println!("{}", v.last().unwrap());
 
-    let x = 2;
-    let y = 2;
-    let z = 2;
-    if (x == y) && (y == z) {
-        println!("it's same for all words.");
-    }
-
-    println!("{}", (10 / 3));
-
-    let mut k = [vec![0], vec![1], vec![3]].concat();
-    println!("{:?}", k);
-
-    let aa = 10usize;
-    let bb = 100usize;
-    println!("{}", bb);
-
-    let vv: Vec<usize> = vec![];
-    println!("{:?}", vv);
-
-    let mut v = vec![false; 10];
-    println!("{:?}", v);
-    v = vec![true; 10];
-    println!("{:?}", v);
-
-    let j = (0..10).map(|x| vec![x]).collect::<Vec<_>>();
-    println!("{:?}", j);
-
-    let ss: HashSet<usize> = (1..10).collect();
-    println!("{:?}", ss);
-
-    let mut graph: HashMap<usize, HashSet<usize>> = HashMap::new();
-    println!("{:?}", graph);
-    graph.insert(0, HashSet::new());
-    println!("{:?}", graph);
-    graph.insert(1, (1..5).collect());
-    println!("{:?}", graph);
-
-    let mut out: HashSet<usize> = [1, 2, 3].iter().cloned().collect();
-    let mut out_2: HashSet<usize> = [2, 3, 4].iter().cloned().collect();
-    println!("{:?}", out);
-    println!("{:?}", out_2);
-    out.retain(|x| !out_2.contains(x));
-    println!("{:?}", out);
-
-    let mut j = vec![0, 1, 2];
-    let mut jj = vec![4, 5, 6];
-
-    swap(&mut j, &mut jj);
-
-    println!("{:?}", j);
+    let h: HashMap<i64, i64> = HashMap::new();
+    h.insert(1, 2);
+    h.insert(3, 4);
+    for 
 }
 
 fn fun_a(s: &Vec<&str>) {
